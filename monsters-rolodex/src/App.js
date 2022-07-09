9fde73830a9aef07d0e279b363b26200e897802a
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-09 17:57:02 +0530

    Component States

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,22 +4,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			name: 'Chirag',
+		};
+	}
 	render() {
 		return (
 			<div className='App'>
 				<header className='App-header'>
 					<img src={logo} className='App-logo' alt='logo' />
-					<p>
-						Edit <code>src/App.js</code> and save to reload.
-					</p>
-					<a
-						className='App-link'
-						href='https://reactjs.org'
-						target='_blank'
-						rel='noopener noreferrer'
-					>
-						Learn React
-					</a>
+					<p>Hii {this.state.name}!</p>
+					<button onClick={this.clickHandler}>Change Name</button>
 				</header>
 			</div>
 		);
