778cd43f1abb712327672f4856ce5e74e73418ab
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-14 17:22:23 +0530

    Cart Context and Toggle Cart Open

--- fashion-hub/src/index.js
+++ fashion-hub/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
+import { CartProvider } from './contexts/cart.context';
 import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
 import './index.scss';
@@ -13,7 +14,9 @@ root.render(
 		<BrowserRouter>
 			<UserProvider>
 				<ProductsProvider>
-					<App />
+					<CartProvider>
+						<App />
+					</CartProvider>
 				</ProductsProvider>
 			</UserProvider>
 		</BrowserRouter>
--- fashion-hub/src/routes/navigation/navigation.component.jsx
+++ fashion-hub/src/routes/navigation/navigation.component.jsx
@@ -5,11 +5,13 @@ import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import './navigation.styles.scss';
 
 const Navigation = () => {
 	const { currentUser } = useContext(UserContext);
+	const { isCartOpen } = useContext(CartContext);
 
 	return (
 		<Fragment>
@@ -34,7 +36,7 @@ const Navigation = () => {
 					)}
 					<CartIcon />
 				</div>
-				<CartDropdown />
+				{isCartOpen && <CartDropdown />}
 			</div>
 			<Outlet />
 		</Fragment>
