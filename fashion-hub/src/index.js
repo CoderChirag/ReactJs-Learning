3a475d9554a2375474cc7d1f58d1453de25e8730
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-07-17 14:24:34 +0530

    Add Firestore Database

--- fashion-hub/src/index.js
+++ fashion-hub/src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { CartProvider } from './contexts/cart.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { UserProvider } from './contexts/user.context';
 import './index.scss';
 
@@ -13,11 +13,11 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<UserProvider>
-				<ProductsProvider>
+				<CategoriesProvider>
 					<CartProvider>
 						<App />
 					</CartProvider>
-				</ProductsProvider>
+				</CategoriesProvider>
 			</UserProvider>
 		</BrowserRouter>
 	</React.StrictMode>
--- fashion-hub/src/utils/firebase/firebase.utils.js
+++ fashion-hub/src/utils/firebase/firebase.utils.js
@@ -10,7 +10,16 @@ import {
 	onAuthStateChanged,
 } from 'firebase/auth';
 
-import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+import {
+	getFirestore,
+	doc,
+	getDoc,
+	setDoc,
+	collection,
+	writeBatch,
+	query,
+	getDocs,
+} from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -34,7 +43,37 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const signInWithGoogleRedirect = () =>
 	signInWithRedirect(auth, provider);
 
-const db = getFirestore();
+export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
+	const collectionRef = collection(db, collectionKey);
+	const batch = writeBatch(db);
+	objectsToAdd.forEach(obj => {
+		const docRef = doc(
+			collectionRef,
+			obj.title.toLowerCase().replace(/\s/g, '-')
+		);
+		batch.set(docRef, obj);
+	});
+	await batch.commit();
+	console.log('done');
+};
+
+export const getCategoriesAndDocuments = async () => {
+	const collectionRef = collection(db, 'categories');
+	const q = query(collectionRef);
+
+	const querySnapshot = await getDocs(q);
+	const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+		const { title, items } = docSnapshot.data();
+		acc[title] = items;
+		return acc;
+	}, {});
+	return categoryMap;
+};
 
 export const createUserDocumentFromAuth = async (
 	userAuth,
