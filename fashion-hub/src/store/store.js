c5e766fb86ad011fce5b4ad2cb42e82e9cdbea74
CoderChirag <chirag0174.be20@chitkara.edu.in>
2022-08-06 02:09:50 +0530

    Cutom Logger Middleware

--- fashion-hub/src/store/store.js
+++ fashion-hub/src/store/store.js
@@ -1,9 +1,25 @@
 import { compose, createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const middleWares = [logger];
+// Custom Middleware
+const loggerMiddleware = store => next => action => {
+	// Function Currying
+	if (!action.type) {
+		return next(action);
+	}
+
+	console.log('type: ', action.type);
+	console.log('payload: ', action.payload);
+	console.log('currentState: ', store.getState());
+
+	next(action);
+
+	console.log('next state: ', store.getState());
+};
+
+const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
